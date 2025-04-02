2667739f8484537876345577abece13e23c0d8b7
unknown <luisfelipeadm.ps@gmail.com>
2025-04-01 21:19:00 -0300

    feat: created service for initial load

--- src/main/java/me/venga/email/emailservice/service/DBService.java
+++ src/main/java/me/venga/email/emailservice/service/DBService.java
@@ -1,12 +1,15 @@
 package me.venga.email.emailservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBService {
 
+    @Value("${spring.mail.username}")
+    private String username;
+
     public void instanciaDB() {
 //        Dados para inserir no banco default
-        System.out.println("Instancia DB");
     }
 }
